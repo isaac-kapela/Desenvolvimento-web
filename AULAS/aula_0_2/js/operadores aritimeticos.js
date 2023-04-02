643969e1438145849eaca9ce90f9544a986e1c9e
isaac-kapela <kapelajoao4@gmail.com>
2023-04-02 12:35:18 -0300

    Adicionamento aula 02| operadores js e primeira pagina html

---
+++ AULAS/aula_0_2/js/operadores aritimeticos.js
@@ -0,0 +1,27 @@
+var numero1 = 3
+var numero2 = 2
+ var salarioAtual = 1000
+ var reajusteSalarial = 10
+ var valorReajustado = salarioAtual * reajusteSalarial / 100
+ var salarioFinal = salarioAtual + valorReajustado
+
+ console.log ("--- Adição---")
+ console.log(numero1," + ", numero2, "=", numero1 + numero2)
+
+ console.log ("--- Subtração---")
+ console.log(numero1," - ", numero2, "=", numero1 - numero2)
+
+ console.log ("--- Mutiplicação---")
+ console.log(numero1," * ", numero2, "=", numero1 * numero2)
+
+ console.log ("--- Divisão---")
+ console.log(numero1," / ", numero2, "=", numero1 / numero2)
+
+ console.log ("--- potencia---")
+ console.log(numero1," ** ", numero2, "=", numero1 ** numero2)
+
+ console.log ("--- Resto da divisão---")
+ console.log(numero1," % ", numero2, "=", numero1 % numero2)
+
+
+
