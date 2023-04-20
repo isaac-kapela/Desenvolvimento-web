8c052655e3a6668957b06ac4acd66ef4ad4d43ef
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-04-20 20:26:10 -0300

    Add files via upload

---
+++ lista 4/js/ExestensãodoHtml_EX_0_2.js
@@ -0,0 +1,9 @@
+var idade = prompt("Digite a sua idade")
+
+    if( idade >= 18){
+        document.writeln(" Você é maior de idade meu nobre")
+    }else if(idade < 18 && idade > 0){
+        document.writeln(" Você é menor de idade meu nobre kkkkk")
+    }else{
+        document.writeln("Erro... tente novamente")
+    }
