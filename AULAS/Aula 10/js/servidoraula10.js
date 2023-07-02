501c1345b458e6c641b23fa466909ae285f11dcb
isaac-kapela <kapelajoao4@gmail.com>
2023-07-02 11:40:31 -0300

    renovei a aula

--- AULAS/Aula 10/js/servidoraula10.js
+++ AULAS/Aula 10/js/servidoraula10.js
@@ -1,24 +1,39 @@
-const express = require ("express")
+const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const port = 8050
-const connectionString ="mongodb+srv://<username>:<password>@appdatabase.oxptztv.mongodb.net/"
-const animal = require ('./models/animal')
-
+const connectionString = "mongodb+srv://admin:admin123@appdatabase.ry0gm2w.mongodb.net/"
+const Animal = require('./models/animal')
+const AnimalSilvestre = require('./models/animaisSilvestres')
+const cors = require('cors')
 app.use(express.json())
+app.use(cors())
 
-app.get("/listar-animal", async (req,res) =>{
+// ENDPOINT
+app.get("/listar-animais", async (req, res) => {
     try {
-        let animais = await animal.find()
-     return res.status(200).json(animais)
+        let animais = await Animal.find()
+        return res.status(200).json(animais)
     } catch (error) {
         return res.status(500).json(error)
     }
 })
-app.post('/adicionar-animal', async (req, res) => {
-    let{nome, raca,peso } = req.body
 
-    let animal ={
+// ENDPOINT
+app.get("/listar-animais-silvestres", async (req, res) => {
+    try {
+        let animais = await AnimalSilvestre.find()
+        return res.status(200).json(animais)
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+})
+
+// ENDPOINT
+app.post('/adicionar-animal', async (req, res) => {
+    let { nome, raca, peso } = req.body
+
+    let animal = {
         nome,
         raca,
         peso
@@ -26,18 +41,18 @@ app.post('/adicionar-animal', async (req, res) => {
 
     try {
         await Animal.create(animal)
-        return res.status(201).json({ message: " Animal cadastrado com sucesso"})
+        return res.status(201).json({ message: "Animal cadastrado com sucesso!" })
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        return res.status(500).json({ message: error.message })
     }
 })
 
-mongoose.connect (connectionString, {
-    dbname: "PETSHOP"
-}).Then(()=> {
-    Console.console.log("MongoDB up!")
+mongoose.connect(connectionString, {
+    dbName: "PETSHOP"
+}).then(() => {
+    console.log("MongoDB UP!")
     console.log(`http://localhost:${port}`)
     app.listen(port)
-}).catch((error) =>{
-    console.log(Error)
+}).catch((error) => {
+    console.log(error)
 })
